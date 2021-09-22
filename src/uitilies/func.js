1d80b66a65c512a27c044592de9ac4f69159a8a4
developer-suhag <developersuhag@gmail.com>
2021-09-22 13:43:53 +0600

    reducer

--- src/uitilies/func.js
+++ src/uitilies/func.js
@@ -10,4 +10,29 @@ function subtrac(x, y) {
   return x - y;
 }
 
+const numbs = [43, 54, 67, 23, 65];
+
+let sum = 0;
+
+for (const num of numbs) {
+  sum += num;
+}
+// console.log(sum);
+
+const reducer = (previous, current) => previous + current;
+
+numbs.reduce(reducer, 0);
+
+const products = [
+  { name: "Alta", price: 50 },
+  { name: "Nail palis", price: 90 },
+  { name: "Makeup box", price: 490 },
+];
+
+const productTotal = products.reduce(
+  (previous, current) => previous + current.price,
+  0
+);
+console.log(productTotal);
+
 export { add, mulitply, subtrac };
